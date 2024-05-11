3812494848d6f0e56adda65769cdb46f25e9bbb5
YH <yhna940@gmail.com>
2024-05-11 22:43:49 +0900

    Add elastic launcher for notebook

--- src/accelerate/test_utils/scripts/test_notebook.py
+++ src/accelerate/test_utils/scripts/test_notebook.py
@@ -18,6 +18,7 @@ Test file to ensure that in general certain situational setups for notebooks wor
 import os
 
 from pytest import raises
+from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
 
 from accelerate import PartialState, notebook_launcher
 from accelerate.test_utils import require_bnb
@@ -27,6 +28,25 @@ from accelerate.utils import is_bnb_available
 def basic_function():
     # Just prints the PartialState
     print(f"PartialState:\n{PartialState()}")
+
+
+NUM_TRIALS = 1
+
+
+def function_with_retry_bug():
+    global NUM_TRIALS
+    if NUM_TRIALS < 3:
+        NUM_TRIALS += 1
+        raise RuntimeError("Trial failed")
+    print(f"PartialState:\n{PartialState()}")
+
+
+def function_with_fractional_bug():
+    state = PartialState()
+    if not state.is_main_process:
+        raise RuntimeError("Fractional bug")
+    state.wait_for_everyone()
+    print(f"PartialState:\n{state}")
 
 
 NUM_PROCESSES = int(os.environ.get("ACCELERATE_NUM_PROCESSES", 1))
@@ -44,12 +64,25 @@ def test_problematic_imports():
         notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES)
 
 
+def test_fault_tolerant():
+    notebook_launcher(function_with_retry_bug, (), num_processes=NUM_PROCESSES, max_restarts=3)
+
+
+def test_fractional_bug():
+    with raises(ChildFailedError, match="Fractional bug"):
+        notebook_launcher(function_with_fractional_bug, (), num_processes=NUM_PROCESSES)
+
+
 def main():
     print("Test basic notebook can be ran")
     test_can_initialize()
     if is_bnb_available():
         print("Test problematic imports (bnb)")
         test_problematic_imports()
+    print("Test fault tolerant")
+    test_fault_tolerant()
+    print("Test fractional bug")
+    test_fractional_bug()
 
 
 if __name__ == "__main__":
