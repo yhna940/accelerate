89472cd3d6d0d910f5b16b732f7d34c15630764f
yhna940 <yhna@lunit.io>
2024-05-16 21:57:16 +0900

    Apply formater

--- src/accelerate/test_utils/scripts/test_notebook.py
+++ src/accelerate/test_utils/scripts/test_notebook.py
@@ -17,16 +17,13 @@ Test file to ensure that in general certain situational setups for notebooks wor
 
 import os
 import time
+from multiprocessing import Queue
 
 from pytest import raises
-from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
 
 from accelerate import PartialState, notebook_launcher
 from accelerate.test_utils import require_bnb
 from accelerate.utils import is_bnb_available
-
-from multiprocessing import Queue
-
 
 
 def basic_function():
@@ -34,11 +31,11 @@ def basic_function():
     print(f"PartialState:\n{PartialState()}")
 
 
-def tough_nut_function(queue:Queue):
+def tough_nut_function(queue: Queue):
     if not queue.full():
         queue.put("knock knock")
         raise RuntimeError("The nut hasn't cracked yet! Try again.")
-    
+
     print(f"PartialState:\n{PartialState()}")
 
 
@@ -65,12 +62,12 @@ def test_c10d_rdzv_backend():
     notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES, rdzv_backend="c10d")
 
 
-def test_fault_tolerant(max_restarts:int=3):
-    Queue = Queue(maxsize=max_restarts-1)
-    notebook_launcher(tough_nut_function, (), num_processes=NUM_PROCESSES, max_restarts=max_restarts)
+def test_fault_tolerant(max_restarts: int = 3):
+    queue = Queue(maxsize=max_restarts - 1)
+    notebook_launcher(tough_nut_function, (queue,), num_processes=NUM_PROCESSES, max_restarts=max_restarts)
 
 
-def test_monitoring(monitor_interval:float=0.01):
+def test_monitoring(monitor_interval: float = 0.01):
     # Assert that the monitor_interval is working
     start_time = time.time()
     notebook_launcher(bipolar_sleep_function, (100,), num_processes=NUM_PROCESSES, monitor_interval=monitor_interval)
