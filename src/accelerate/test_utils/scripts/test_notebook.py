931c661d608186e817d819db81c4cf37103e5edf
yhna940 <yhna@lunit.io>
2024-05-16 21:50:16 +0900

    Add test case

--- src/accelerate/test_utils/scripts/test_notebook.py
+++ src/accelerate/test_utils/scripts/test_notebook.py
@@ -16,6 +16,7 @@ Test file to ensure that in general certain situational setups for notebooks wor
 """
 
 import os
+import time
 
 from pytest import raises
 from torch.distributed.elastic.multiprocessing.errors import ChildFailedError
@@ -24,29 +25,29 @@ from accelerate import PartialState, notebook_launcher
 from accelerate.test_utils import require_bnb
 from accelerate.utils import is_bnb_available
 
+from multiprocessing import Queue
+
+
 
 def basic_function():
     # Just prints the PartialState
     print(f"PartialState:\n{PartialState()}")
 
 
-NUM_TRIALS = 1
-
-
-def function_with_retry_bug():
-    global NUM_TRIALS
-    if NUM_TRIALS < 3:
-        NUM_TRIALS += 1
-        raise RuntimeError("Trial failed")
+def tough_nut_function(queue:Queue):
+    if not queue.full():
+        queue.put("knock knock")
+        raise RuntimeError("The nut hasn't cracked yet! Try again.")
+    
     print(f"PartialState:\n{PartialState()}")
 
 
-def function_with_fractional_bug():
+def bipolar_sleep_function(sleep_sec: int):
     state = PartialState()
-    if not state.is_main_process:
-        raise RuntimeError("Fractional bug")
-    state.wait_for_everyone()
-    print(f"PartialState:\n{state}")
+    if state.process_index % 2 == 0:
+        time.sleep(sleep_sec)
+    else:
+        raise RuntimeError("sad because i throw")
 
 
 NUM_PROCESSES = int(os.environ.get("ACCELERATE_NUM_PROCESSES", 1))
@@ -54,6 +55,26 @@ NUM_PROCESSES = int(os.environ.get("ACCELERATE_NUM_PROCESSES", 1))
 
 def test_can_initialize():
     notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES)
+
+
+def test_static_rdzv_backend():
+    notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES, rdzv_backend="static")
+
+
+def test_c10d_rdzv_backend():
+    notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES, rdzv_backend="c10d")
+
+
+def test_fault_tolerant(max_restarts:int=3):
+    Queue = Queue(maxsize=max_restarts-1)
+    notebook_launcher(tough_nut_function, (), num_processes=NUM_PROCESSES, max_restarts=max_restarts)
+
+
+def test_monitoring(monitor_interval:float=0.01):
+    # Assert that the monitor_interval is working
+    start_time = time.time()
+    notebook_launcher(bipolar_sleep_function, (100,), num_processes=NUM_PROCESSES, monitor_interval=monitor_interval)
+    assert time.time() - start_time < 2 * monitor_interval, "Monitoring is not working"
 
 
 @require_bnb
@@ -64,25 +85,20 @@ def test_problematic_imports():
         notebook_launcher(basic_function, (), num_processes=NUM_PROCESSES)
 
 
-def test_fault_tolerant():
-    notebook_launcher(function_with_retry_bug, (), num_processes=NUM_PROCESSES, max_restarts=3)
-
-
-def test_fractional_bug():
-    with raises(ChildFailedError, match="Fractional bug"):
-        notebook_launcher(function_with_fractional_bug, (), num_processes=NUM_PROCESSES)
-
-
 def main():
     print("Test basic notebook can be ran")
     test_can_initialize()
+    print("Test static rendezvous backend")
+    test_static_rdzv_backend()
+    print("Test c10d rendezvous backend")
+    test_c10d_rdzv_backend()
+    print("Test fault tolerant")
+    test_fault_tolerant()
+    print("Test monitoring")
+    test_monitoring()
     if is_bnb_available():
         print("Test problematic imports (bnb)")
         test_problematic_imports()
-    print("Test fault tolerant")
-    test_fault_tolerant()
-    print("Test fractional bug")
-    test_fractional_bug()
 
 
 if __name__ == "__main__":
